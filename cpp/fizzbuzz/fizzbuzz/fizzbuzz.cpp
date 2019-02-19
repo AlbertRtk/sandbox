e31019b6c108ef7cbbf771f11f98c2865bb6e3ff
Albert <ratajczakalbert@gmail.com>
2019-02-19 21:39:28 +0100

    adding fizzbuzz

---
+++ cpp/fizzbuzz/fizzbuzz/fizzbuzz.cpp
@@ -0,0 +1,30 @@
+// fizzbuzz.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+/*
+Write a program that prints the numbers from 1 to 100. 
+But for multiples of three print “Fizz” instead of the number 
+and for the multiples of five print “Buzz”. For numbers which
+are multiples of both three and five print “FizzBuzz”.
+*/
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+void fizzbuzz(int i) {
+	if ( (i % 3 == 0) && (i % 5 ==0) ) { cout << "FizzBuzz"; }
+	else if (i % 3 == 0) { cout << "Fizz"; }
+	else if (i % 5 == 0) { cout << "Buzz"; }
+	else { cout << i; }
+	cout << endl;
+}
+
+int main()
+{
+	for (int i = 1; i <= 100; i++) {
+		fizzbuzz(i);
+	}
+	return 0;
+}
+
